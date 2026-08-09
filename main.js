51e94c28401a24948365832220f0733c7417f383
JonaCodes <jonathanfarache@gmail.com>
2026-08-09 16:09:30 +0300

    remove isPositiveReview and update expected output

--- main.js
+++ main.js
@@ -23,10 +23,6 @@ const askGemini = async (model, userPrompt) => {
   }
 };
 
-const isPositiveReview = (llmResult) => {
-  return llmResult.toLowerCase().includes("positive"); // naive implementation - we'll improve next lesson
-};
-
 const run = async () => {
   const model = "gemini-2.5-flash";
   const userPrompt = `
@@ -41,11 +37,7 @@ const run = async () => {
   console.log(`Output tokens: ${usageMetadata.candidatesTokenCount}`);
   console.log("\n---\n");
 
-  if (isPositiveReview(responseText)) {
-    console.log("Celebrate.");
-  } else {
-    console.log("Alert the author!");
-  }
+  console.log(responseText);
 };
 
 run();
